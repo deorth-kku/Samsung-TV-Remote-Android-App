94213b2f8169be8081031f0a284e607f551c5662
deorth-kku <deorth@outlook.com>
2026-06-03 12:36:15 +0800

    add some ignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
